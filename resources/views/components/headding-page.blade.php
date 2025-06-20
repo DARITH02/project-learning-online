2a0a0856f5317d360f42902d3a006707be9009b0
DARITH02 <chandarith002@gmail.com>
2025-06-20 13:17:23 +0700

    udpate ui

--- resources/views/components/headding-page.blade.php
+++ resources/views/components/headding-page.blade.php
@@ -1,4 +1,4 @@
-<nav {{$attributes->merge(['class' => "w-full px-5 py-3 shadow bg-blue-500"]) }}>
+{{-- <nav {{$attributes->merge(['class' => "w-full px-5 py-3 shadow bg-blue-500"]) }}>
     <div class="w-full flex justify-between items-center gap-2 text-2xl">
         <h3 class="font-bold">
             {{$headding}}
@@ -8,11 +8,21 @@
                 {{$title1}}
             </li>
             <li>
-                {{$title2}}
+
             </li>
             <li>
-                {{$title3}}
+
             </li>
         </ul>
     </div>
-</nav>
+</nav> --}}
+<div {{$attributes->merge(['class' => 'bg-mode border-b border-gray-200 px-6 py-4']) }}>
+    <div class="flex items-center justify-between">
+        <h1 class="text-2xl font-bold ">{{$headding}}</h1>
+        <nav class="text-sm ">
+            <a href="#" class="text-blue-600 "> {{$title1}}</a>
+            <span class="mx-2">/</span>
+            <span class=""> {{$title2}}</span>
+        </nav>
+    </div>
+</div>
